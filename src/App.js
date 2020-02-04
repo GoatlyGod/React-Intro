4cbd298af4c2a03ce2e53bdb47a78941f806efa7
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-04 12:10:18 -0500

    side by side jumbotron

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from './components/Button'
+import Jumbotron from './components/Jumbotron'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Button />
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <Jumbotron />
+          </div>
+
+          <div className="col">
+          <Jumbotron />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+    return (
+        <div>
+            <button  type="button" className="btn btn-primary">Click here</button>
+        </div>
+    )
+};
+
+export default Button;
